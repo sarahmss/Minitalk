d86577aaaa667aa318b9941bce49802c5412b7fb
sarahmss <sarahmodestos@gmail.com>
2021-09-21 14:21:32 -0300

    first commit

---
+++ includes/minitalk.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smodesto <smodesto@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/17 18:56:26 by smodesto          #+#    #+#             */
+/*   Updated: 2021/09/20 21:08:21 by smodesto         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include "../libraries/libft/libft.h"
+
+
+#endif
---
+++ source/main.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smodesto <smodesto@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/18 17:58:28 by smodesto          #+#    #+#             */
+/*   Updated: 2021/09/20 19:15:39 by smodesto         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minitalk.h"
+
+int	main(int argc, char **argv)
+{
+	if (argc != 2)
+	{
+		ft_printf("Insert only one file: ./fdf <MAP_FILE> \n");
+		exit (0);
+	}
+	return (0);
+}
