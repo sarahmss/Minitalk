3456e99b4bd086d9dafee7e60dfa77457bfe7275
sarahmss <sarahmodestos@gmail.com>
2021-09-29 11:29:17 -0300

    test

--- source/source_c/client.c
+++ source/source_c/client.c
@@ -3,62 +3,67 @@
 /*                                                        :::      ::::::::   */
 /*   client.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: smodesto <smodesto@student.42.fr>          +#+  +:+       +#+        */
+/*   By: smodesto <smodesto@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/23 14:11:28 by smodesto          #+#    #+#             */
-/*   Updated: 2021/09/25 19:55:57 by smodesto         ###   ########.fr       */
+/*   Updated: 2021/09/29 11:27:40 by smodesto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/minitalk.h"
-
-static void send_signals(int pid, char *message)
-{
-    int bitshift;
-    int i;
-    int max;
-    
-    i = 0;
-    bitshift = -1;
-    max = 0x80;
-    while(message[i])
-    {
-        while(++bitshift < 8)
-        {
-            if (message[i] & max >> bitshift)
-            {
-                if (kill(pid, SIGUSR2) == -1)
-                    exit(1);                 
-            }
-            else
-            {
-                if(kill(pid, SIGUSR1) == -1)
-                    exit(1);                 
-            }
-            usleep(3);
-        }
-        i++;
-        bitshift = -1;
-        max = 0x80;
-    }
-
-}
+#include "../../includes/minitalk.h"
 
 static void sucess(int sig)
 {
-    ft_printf("Data has been received with signal %d\n", sig);
+	if (sig == SIGUSR2)
+		ft_printf("Data has been received with signal %d\n", sig);
 }
+
+static void send_signals(int pid, char c)
+{
+	int	bits;
+
+	bits = 0;
+	while (bits < 8)
+	{
+		if (c % 2 == 0 || c == 0)
+		{
+			if (kill(pid, SIGUSR1) == -1)
+				exit (1);
+			usleep(1000);
+		}
+		else
+		{
+			if (kill(pid, SIGUSR2) == -1)
+				exit (1);
+			usleep(1000);
+		}
+		c /= 2;
+		bits++;
+	}
+}
+
+
 int main(int argc, char *argv[])
 {
-    int     pid;
-    
-    if (argc != 3)
+	int					pid;
+	char				*message;
+	struct sigaction	sa;
+
+	ft_bzero(&sa, sizeof(sa));
+	sa.sa_handler = sucess;
+	if (argc != 3)
 	{
 		ft_printf("Client: invalid arguments. Try ./client <pid> <\"str>\"\n");
 		exit (1);
 	}
-    pid = ft_atoi(argv[1]);
-    send_signals(pid, argv[2]);
-    signal(SIGUSR1, sucess);
-    return (0);
+	pid = ft_atoi(argv[1]);
+	message = argv[2];
+	while (*message)
+	{
+		send_signals(pid, *message);
+		message++;
+	}
+	sigaction(SIGUSR2, &sa, NULL);
+	send_signals(pid, '\0');
+	return (0);
 }
--- source/source_s/server.c
+++ source/source_s/server.c
@@ -3,48 +3,52 @@
 /*                                                        :::      ::::::::   */
 /*   server.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: smodesto <smodesto@student.42.fr>          +#+  +:+       +#+        */
+/*   By: smodesto <smodesto@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/23 14:11:50 by smodesto          #+#    #+#             */
-/*   Updated: 2021/09/25 19:57:07 by smodesto         ###   ########.fr       */
+/*   Updated: 2021/09/29 11:28:13 by smodesto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/minitalk.h"
+#include "../../includes/minitalk.h"
 
-static void handler_sigusr(int sig)
+void handler_sigusr(int sig, siginfo_t *info_signal, void *context)
 {
-    static char    c = 0xFF;
-    static int     bits = 0;
-    
-    if (sig == SIGUSR1)
-	{ 
-        ft_printf("0", sig);
-        c ^= 0x80 >> bits;
-    }
-   if (sig == SIGUSR2)
-	{ 
-        ft_printf("1", sig);
-        c |= 0x80 >> bits;
-    }
-    bits++;
-    if (bits == 8)
-    {
-        ft_printf("-> %c\n", c);
-        bits = 0;
-        c = 0xFF;
-    }
+	static unsigned int	caracter;
+	static unsigned int	bits;
+
+	(void)context;
+	if (sig == SIGUSR2)
+		caracter += 1 << bits;
+	bits++;
+	if (bits == 8)
+	{
+		if (caracter == 0)
+		{
+			printf("\n");
+			kill(info_signal->si_pid, SIGUSR2);
+		}
+		else
+			printf("%c", caracter);
+		bits = 0;
+		caracter = 0;
+	}
 }
+
 
 int main(void)
 {
-    pid_t       pid;
-    
-    pid = getpid();
-    ft_printf("PID: %d\n", pid);
-    signal(SIGUSR1, handler_sigusr);
-    signal(SIGUSR2, handler_sigusr);
-    while (1)
-        pause();
-   return (0); 
+	int					pid;
+	struct sigaction	sa;
+
+	pid = getpid();
+	ft_bzero(&sa, sizeof(sa));
+	sa.sa_flags = SA_SIGINFO;
+	sa.sa_sigaction = handler_sigusr;
+	ft_printf("PID: %d\n", pid);
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
+	while (1)
+		pause();
+	return (0);
 }
